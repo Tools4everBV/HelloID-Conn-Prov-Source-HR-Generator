476c97973ac0f2d512132a204a4a707e3b92d7ab
Kees Visser <k.visser@tools4ever.com>
2023-07-19 11:46:33 +0200

    Proper department script

--- Departments.ps1
+++ Departments.ps1
@@ -1,6 +1,22 @@
-$departments = HrGenerator -Type Department -NrOfPersons 1 -NrOfContracts 1 -NrOfDepartments 10 -PercentageDepartmentsWithParentDepartments 50 -PercentageInactivePersons 20 -Convention P -ContractCalculationType Employments -Seed 1;
+$c = $configuration | ConvertFrom-Json
+
+Write-Verbose -Verbose "Generating departments";
+
+$departments = HrGenerator `
+            -Type Department `
+            -NrOfPersons $c.nrOfPersons `
+            -NrOfContracts $c.nrOfContracts `
+            -NrOfDepartments $c.nrOfDepartments `
+            -PercentageDepartmentsWithParentDepartments $c.percentageOfDepartmentsWithParentDepartments `
+            -PercentageInactivePersons $c.percentageOfInactivePersons `
+            -Convention $c.convention `
+            -ContractCalculationType $c.contractCalculationType `
+            -Seed $c.seed;
+
+Write-Verbose -Verbose "Sending departments to HelloID";
 foreach ($department in $departments)
 {
     Write-Output $department | ConvertTo-Json -Depth 10
 }
 Write-Verbose -Verbose "Department import completed";
+Write-Verbose -Verbose "Person import completed";
